06a26fb02f327f5d6404dd0c2aa6ac0640721c29
NatashaTheRobot <nmurashev@gmail.com>
2025-07-17 06:18:15 +0530

    Initial Commit

---
+++ GeminiBatch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GeminiBatch
+//
+//  Created by Natasha Murashev on 7/17/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GeminiBatch/GeminiBatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeminiBatchApp.swift
+//  GeminiBatch
+//
+//  Created by Natasha Murashev on 7/17/25.
+//
+
+import SwiftUI
+
+@main
+struct GeminiBatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
