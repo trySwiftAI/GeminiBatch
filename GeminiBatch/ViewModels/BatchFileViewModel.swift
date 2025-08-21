f3c7534b1b4e7d731b997785cdbf8bc4a3cdf2af
NatashaTheRobot <nmurashev@gmail.com>
2025-08-21 12:29:53 +0530

    refactor

--- GeminiBatch/ViewModels/BatchFileViewModel.swift
+++ GeminiBatch/ViewModels/BatchFileViewModel.swift
@@ -129,8 +129,10 @@ extension BatchFileViewModel {
         
         // Determine action based on job status
         switch batchJob.jobStatus {
-        case .notStarted, .started, .fileUploaded, .pending, .running, .succeeded:
+        case .notStarted, .started, .fileUploaded, .pending, .succeeded:
             return .run
+        case .running:
+            return .running
         case .unspecified, .failed, .cancelled, .expired:
             return .retry
         case .jobFileDownloaded:
